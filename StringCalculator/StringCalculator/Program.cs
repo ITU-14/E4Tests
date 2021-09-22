a15a991d70c87b915454a626fd6030dbfc6ebec2
Khino <ricoeurkhino1@gmail.com>
2021-09-22 20:17:57 +0400

    [Test 2 - Console App] E4 - String Calculator.

---
+++ StringCalculator/StringCalculator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Add("1,-2\n3,-5"));
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            Console.ReadKey();
+        }
+
+        /*
+         * Time and Space complexity: O(n) where n is the length of the inputNumbers String.
+         */
+        public static int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers)) return 0;
+            
+            numbers = HandleDelimiters(numbers, out char delimiter);
+
+            numbers = numbers.Replace('\n', delimiter);
+
+            // in case we have the inputNumbers as follows "1,\n", I assume we can trim the \n with the comma then we can have 1 as a result.
+            // but if we have the inputNumbers as follows "1,\n3,5", I assume we have an exception saying input error.
+
+            for(var k = 0; k < numbers.Length-1;k ++)
+            {
+                if (numbers[k] == numbers[k + 1] && numbers[k] == delimiter) throw new Exception("input error!");
+            }
+
+            var nums = numbers.Split(delimiter);
+
+            var exceptionNumbers = string.Empty;
+
+            var result = 0;
+
+            int num;
+
+            foreach (var numberString in nums)
+            {
+                if (int.TryParse(numberString, out num) && num < 0) {
+                    exceptionNumbers = string.Concat(exceptionNumbers, num, ",");
+
+                    continue;
+                }
+
+                result += num;
+            }
+
+            if (!string.IsNullOrEmpty(exceptionNumbers)) throw new Exception($"negatives not allowed [{exceptionNumbers.TrimEnd(',')}]");
+
+            return result;
+        }
+
+        static string HandleDelimiters(string numbers, out char delimiter)
+        {
+            if (!numbers.StartsWith("//")) {
+                delimiter = ',';
+                return numbers;
+            }
+
+            delimiter = numbers[2];
+
+            return numbers.Substring(4);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorUnitTest/StringCalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+using System;
+
+namespace StringCalculatorUnitTest
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void InputNumbersIsEmpty()
+        {
+            Assert.AreEqual(0, Program.Add(""));
+        }
+
+        [TestMethod]
+        public void InputNumbersAddition()
+        {
+            Assert.AreEqual(6, Program.Add("1,2,3"));
+        }
+
+        [TestMethod]
+        public void InputNumbersWithNewLineBetweenNumbers()
+        {
+            Assert.AreEqual(6, Program.Add("1,2\n3"));
+        }
+
+        [TestMethod]
+        public void InputWithDifferentDelimiters()
+        {
+            Assert.AreEqual(11, Program.Add("//;\n1;2\n3;5"));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnNegativeNumbers()
+        {
+            Assert.ThrowsException<Exception>(() => Program.Add("1,-2\n3,-5"));
+        }
+    }
+}
